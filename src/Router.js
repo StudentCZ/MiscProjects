58403346958a0dbac49ab0809a2a10a9259ea251
Chao Zhou <Czhou0k@gmail.com>
2022-10-20 11:31:13 -0400

    added boilerplate for Routes

--- src/Router.js
+++ src/Router.js
@@ -1,9 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import style from './Router.module.css';
+import App from './App';
 
 const Router = () => {
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<App />}></Route>
+        </Routes>
+      </BrowserRouter>
+
       <nav className={style.navBar}>
         <ul className={style.li}>
           <li>HomePage</li>
