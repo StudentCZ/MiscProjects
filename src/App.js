5c2827943e3ce5de2da44633175aef78c99aac33
Chao Zhou <Czhou0k@gmail.com>
2022-10-20 11:57:00 -0400

    added a quote component and imported into app with route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
-import NavBar from './NavBar';
 import Home from './Home';
+import Quote from './Random_Quote_Generator/Quote';
 
 function App() {
   return (
     <div className='App'>
       <Routes>
         <Route path='/' element={<Home />}></Route>
+        <Route path='/quote' element={<Quote />}></Route>
       </Routes>
     </div>
   );
