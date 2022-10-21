5aad24b9d39abab78c455d250f8bbc436f77d61e
Chao Zhou <Czhou0k@gmail.com>
2022-10-21 17:48:14 -0400

    removed strict mode that cause api rendering twice

--- src/Random_Quote_Generator/Quote.js
+++ src/Random_Quote_Generator/Quote.js
@@ -3,13 +3,16 @@ import Home from '../Home';
 import { useState, useEffect } from 'react';
 
 const Quote = () => {
+  const [loading, setLoading] = useState(false);
   const [quotes, setQuotes] = useState([]);
   const [randomQuote, setRandomQuote] = useState([]);
 
   useEffect(() => {
+    setLoading(true);
     fetch('https://animechan.vercel.app/api/random')
       .then((response) => response.json())
       .then((quote) => setQuotes(quote));
+    setLoading(false);
   }, []);
 
   return (
