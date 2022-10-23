24fa48dd4a8676084e019181989935d4fb5ec821
Chao Zhou <Czhou0k@gmail.com>
2022-10-23 12:49:45 -0400

    imported markdown to routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import Quote from './Random_Quote_Generator/Quote';
+import Markdown from './Markdown_Previewer/Markdown';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/quote' element={<Quote />}></Route>
+        <Route path='/markdown' element={<Markdown />}></Route>
       </Routes>
     </div>
   );
