c2a989bcbf0bc78813248dfd7dafa11d95c19f5a
Chao Zhou <Czhou0k@gmail.com>
2022-10-24 22:33:16 -0400

    imported home component

--- src/Markdown_Previewer/Markdown.js
+++ src/Markdown_Previewer/Markdown.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
+import Home from '../Home';
 
 const Markdown = () => {
   const [text, setText] = useState('');
 
   return (
     <div>
+      <Home />
       <h1>Markdown Previewer</h1>
       <textarea
         id='editor'
