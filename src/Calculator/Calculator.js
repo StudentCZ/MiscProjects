950e8b0b3a57a47a70bf66fb0d27dc1bf09e1190
Chao Zhou <Czhou0k@gmail.com>
2022-11-09 22:49:35 -0500

    added button and color

--- src/Calculator/Calculator.js
+++ src/Calculator/Calculator.js
@@ -6,6 +6,9 @@ const Calculator = () => {
     <div className='calculator card'>
       <input type='text' className='calculator screen z-depth-1' value='0' />
       <div class='calculator-keys'></div>
+      <button type='button' class='btn btn-info waves-effect'>
+        7
+      </button>
     </div>
   );
 };
