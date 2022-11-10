e4b232aeb4f554e7edc703bc76466fb867ac6d21
Chao Zhou <Czhou0k@gmail.com>
2022-11-09 23:24:16 -0500

    change all class to classname

--- src/Calculator/Calculator.js
+++ src/Calculator/Calculator.js
@@ -5,32 +5,32 @@ const Calculator = () => {
   return (
     <div className='calculator card'>
       <input type='text' className='calculator-screen z-depth-1' value='0' />
-      <div class='calculator-keys'>
-        <button type='button' class='btn btn-info waves-effect'>
+      <div className='calculator-keys'>
+        <button type='button' className='btn btn-info waves-effect'>
           7
         </button>
-        <button type='button' class='btn btn-info waves-effect'>
+        <button type='button' className='btn btn-info waves-effect'>
           8
         </button>
-        <button type='button' class='btn btn-info waves-effect'>
+        <button type='button' className='btn btn-info waves-effect'>
           9
         </button>
-        <button type='button' class='btn btn-info waves-effect'>
+        <button type='button' className='btn btn-info waves-effect'>
           4
         </button>
-        <button type='button' class='btn btn-info waves-effect'>
+        <button type='button' className='btn btn-info waves-effect'>
           5
         </button>
-        <button type='button' class='btn btn-info waves-effect'>
+        <button type='button' className='btn btn-info waves-effect'>
           6
         </button>
-        <button type='button' class='btn btn-info waves-effect'>
+        <button type='button' className='btn btn-info waves-effect'>
           1
         </button>
-        <button type='button' class='btn btn-info waves-effect'>
+        <button type='button' className='btn btn-info waves-effect'>
           2
         </button>
-        <button type='button' class='btn btn-info waves-effect'>
+        <button type='button' className='btn btn-info waves-effect'>
           3
         </button>
       </div>
