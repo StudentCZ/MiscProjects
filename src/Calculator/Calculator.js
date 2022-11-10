10df980385b6248e1d6a229218e3627f18f4918a
Chao Zhou <Czhou0k@gmail.com>
2022-11-10 17:08:00 -0500

    added bootstraprow

--- src/Calculator/Calculator.js
+++ src/Calculator/Calculator.js
@@ -6,18 +6,20 @@ const Calculator = () => {
     <div className='calculator card' style={{ width: '400px' }}>
       <input type='text' className='calculator-screen z-depth-1' value='0' />
       <div className='calculator-keys'>
-        <button type='button' className='btn btn-info waves-effect'>
-          7
-        </button>
-        <button type='button' className='btn btn-info waves-effect'>
-          8
-        </button>
-        <button type='button' className='btn btn-info waves-effect'>
-          9
-        </button>
-        <button type='button' className='btn btn-info waves-effect'>
-          4
-        </button>
+        <div className='row'>
+          <button type='button' className='btn btn-info waves-effect'>
+            7
+          </button>
+          <button type='button' className='btn btn-info waves-effect'>
+            8
+          </button>
+          <button type='button' className='btn btn-info waves-effect'>
+            9
+          </button>
+          <button type='button' className='btn btn-info waves-effect'>
+            4
+          </button>
+        </div>
         <button type='button' className='btn btn-info waves-effect'>
           5
         </button>
