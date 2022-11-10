d0622489181dd365871f335f17e983deb96342f2
Chao Zhou <Czhou0k@gmail.com>
2022-11-09 23:16:40 -0500

    changed calculator css to module css

--- src/Calculator/Calculator.js
+++ src/Calculator/Calculator.js
@@ -4,35 +4,36 @@ import { useState } from 'react';
 const Calculator = () => {
   return (
     <div className='calculator card'>
-      <input type='text' className='calculator screen z-depth-1' value='0' />
-      <div class='calculator-keys'></div>
-      <button type='button' class='btn btn-info waves-effect'>
-        7
-      </button>
-      <button type='button' class='btn btn-info waves-effect'>
-        8
-      </button>
-      <button type='button' class='btn btn-info waves-effect'>
-        9
-      </button>
-      <button type='button' class='btn btn-info waves-effect'>
-        4
-      </button>
-      <button type='button' class='btn btn-info waves-effect'>
-        5
-      </button>
-      <button type='button' class='btn btn-info waves-effect'>
-        6
-      </button>
-      <button type='button' class='btn btn-info waves-effect'>
-        1
-      </button>
-      <button type='button' class='btn btn-info waves-effect'>
-        2
-      </button>
-      <button type='button' class='btn btn-info waves-effect'>
-        3
-      </button>
+      <input type='text' className='calculator-screen z-depth-1' value='0' />
+      <div class='calculator-keys'>
+        <button type='button' class='btn btn-info waves-effect'>
+          7
+        </button>
+        <button type='button' class='btn btn-info waves-effect'>
+          8
+        </button>
+        <button type='button' class='btn btn-info waves-effect'>
+          9
+        </button>
+        <button type='button' class='btn btn-info waves-effect'>
+          4
+        </button>
+        <button type='button' class='btn btn-info waves-effect'>
+          5
+        </button>
+        <button type='button' class='btn btn-info waves-effect'>
+          6
+        </button>
+        <button type='button' class='btn btn-info waves-effect'>
+          1
+        </button>
+        <button type='button' class='btn btn-info waves-effect'>
+          2
+        </button>
+        <button type='button' class='btn btn-info waves-effect'>
+          3
+        </button>
+      </div>
     </div>
   );
 };
