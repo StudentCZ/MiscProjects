4cd01f3d46b072c9ba510deded3f6db708b60309
Chao Zhou <Czhou0k@gmail.com>
2022-11-09 22:37:22 -0500

    added another div for calculators keys

--- src/Calculator/Calculator.js
+++ src/Calculator/Calculator.js
@@ -5,6 +5,7 @@ const Calculator = () => {
   return (
     <div className='calculator card'>
       <input type='text' className='calculator screen z-depth-1' value='0' />
+      <div class='calculator-keys'></div>
     </div>
   );
 };
