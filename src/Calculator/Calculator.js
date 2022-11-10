fe57ab8581c0a7f803f76457847c79cff1531673
Chao Zhou <Czhou0k@gmail.com>
2022-11-09 22:59:33 -0500

    added more numbers

--- src/Calculator/Calculator.js
+++ src/Calculator/Calculator.js
@@ -9,6 +9,30 @@ const Calculator = () => {
       <button type='button' class='btn btn-info waves-effect'>
         7
       </button>
+      <button type='button' class='btn btn-info waves-effect'>
+        8
+      </button>
+      <button type='button' class='btn btn-info waves-effect'>
+        9
+      </button>
+      <button type='button' class='btn btn-info waves-effect'>
+        4
+      </button>
+      <button type='button' class='btn btn-info waves-effect'>
+        5
+      </button>
+      <button type='button' class='btn btn-info waves-effect'>
+        6
+      </button>
+      <button type='button' class='btn btn-info waves-effect'>
+        1
+      </button>
+      <button type='button' class='btn btn-info waves-effect'>
+        2
+      </button>
+      <button type='button' class='btn btn-info waves-effect'>
+        3
+      </button>
     </div>
   );
 };
